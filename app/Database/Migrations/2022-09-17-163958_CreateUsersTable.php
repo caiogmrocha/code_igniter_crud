46abb13a1e73b522df72115888a88728ab90a27d
caiogmrocha <caioguilherme739@gmail.com>
2022-09-17 14:14:49 -0300

    chore: create CreateUsersTable migration

---
+++ app/Database/Migrations/2022-09-17-163958_CreateUsersTable.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+use CodeIgniter\Database\RawSql;
+
+class CreateUsersTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'name' => [
+                'type'           => 'VARCHAR',
+                'constraint'     => 244
+            ],
+            'email' => [
+                'type'           => 'VARCHAR',
+                'constraint'     => 244,
+                'unique'         => true
+            ],
+            'password' => [
+                'type'           => 'VARCHAR',
+                'constraint'     => 244
+            ],
+            'created_at' => [
+                'type'    => 'TIMESTAMP',
+                'default' => new RawSql('CURRENT_TIMESTAMP')
+            ],
+            'updated_at' => [
+                'type'    => 'TIMESTAMP',
+                'default' => new RawSql('CURRENT_TIMESTAMP')
+            ]
+        ]);
+
+        $this->forge->addPrimaryKey('id');
+
+        $this->forge->createTable('users');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('users');
+    }
+}
